092cbaa6dccb20f809f124b555661dd20c92364e
hoseking <shosking@venturemedia.com>
2015-03-18 09:36:33 -0700

    Add file saving to recording controller

--- IntuneLab/RecordViewController.mm
+++ IntuneLab/RecordViewController.mm
@@ -13,10 +13,11 @@ static const float kSampleRate = 44100;
 static const NSTimeInterval kWaveformMaxDuration = 5;
 static const std::size_t kPacketSize = 1024;
 
-@interface RecordViewController ()
+@interface RecordViewController () <UIAlertViewDelegate, UITextFieldDelegate>
 
 @property(nonatomic, weak) IBOutlet VMWaveformView* waveformView;
 @property(nonatomic, weak) IBOutlet UIButton* startStopButton;
+@property(nonatomic, weak) IBOutlet UITextField *filenameTextField;
 
 @property(nonatomic) std::shared_ptr<MicrophoneModule> microphoneModule;
 @property(nonatomic) std::shared_ptr<AccumulatorModule> accumulatorModule;
@@ -36,15 +37,28 @@ static const std::size_t kPacketSize = 1024;
 
 - (IBAction)startStop {
     if (!_microphoneModule || !_microphoneModule->isRunning())
-        [self start];
+        [self tryStart];
     else
         [self stop];
+}
+
+- (void)tryStart {
+    if ([self recordingFileExists]) {
+        [[[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"File \"%@\" exists", [self recordingFilename]]
+                                    message:@"Overwrite?"
+                                   delegate:self
+                          cancelButtonTitle:@"NO"
+                          otherButtonTitles:@"YES", nil] show];
+        return;
+    }
+
+    [self start];
 }
 
 - (void)start {
     if (!_microphoneModule)
         [self initializeModuleGraph];
-    
+
     _microphoneModule->start();
     [self.startStopButton setTitle:@"Stop" forState:UIControlStateNormal];
 }
@@ -52,9 +66,8 @@ static const std::size_t kPacketSize = 1024;
 - (void)stop {
     if (!_microphoneModule)
         return;
-    
-    _microphoneModule->stop();
 
+    _microphoneModule->stop();
     [self.startStopButton setTitle:@"Record" forState:UIControlStateNormal];
 }
 
@@ -71,22 +84,55 @@ static const std::size_t kPacketSize = 1024;
 }
 
 - (void)initializeModuleGraph {
-    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString* documentsPath = [paths objectAtIndex:0];
-    NSString* destination = [documentsPath stringByAppendingPathComponent:@"microphone.caf"];
-    _fileWriter.reset(new SaveToFileModule(destination.UTF8String, kSampleRate));
-
+    _fileWriter.reset(new SaveToFileModule([self recordingFile].UTF8String, kSampleRate));
+    
     _waveformView.sampleRate = kSampleRate;
     _waveformView.duration = kWaveformMaxDuration;
 
     _microphoneModule.reset(new MicrophoneModule);
-    _microphoneModule->onDataAvailable([self](std::size_t size) {
-        [self step];
+    __weak RecordViewController* wself = self;
+    _microphoneModule->onDataAvailable([wself](std::size_t size) {
+        [wself step];
     });
 
     std::size_t capacity = kSampleRate * kWaveformMaxDuration;
     _accumulatorModule.reset(new AccumulatorModule(capacity));
     _accumulatorModule->setSource(_microphoneModule);
+}
+
+
+#pragma mark - Save file location
+
+- (NSString*)recordingFilename {
+    NSString* filename = @"microphone.caf";
+    if (_filenameTextField.text.length > 0)
+        filename = [_filenameTextField.text stringByAppendingString:@".caf"];
+    return filename;
+}
+
+- (NSString*)recordingFile {
+    NSString* documentsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+    return [documentsPath stringByAppendingPathComponent:[self recordingFilename]];
+}
+
+- (BOOL)recordingFileExists {
+    return [[NSFileManager defaultManager] fileExistsAtPath:[self recordingFile] isDirectory:nil];
+}
+
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+#pragma mark - UIAlertViewDelegate
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1)
+        [self start];
 }
 
 @end
