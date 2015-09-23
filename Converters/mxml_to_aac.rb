36b76c0f4e1f235cea17cce8e5737b9cae1479db
Alejandro Isaza <aisaza@venturemedia.com>
2015-09-23 11:47:25 -0700

    Add ability to adjust gain to converter

--- Converters/mxml_to_aac.rb
+++ Converters/mxml_to_aac.rb
@@ -20,23 +20,44 @@ end
 def midi_to_wav(dir, name)
   input = dir + File::SEPARATOR + name + ".mid"
   sound_fonts = [
-    "sf/AcousticGrandPiano_YDP.sf2",
-    "sf/Arachno.sf2",
-    "sf/FluidR3_GM2-2.sf2",
-    "sf/GeneralUser_GS_MuseScore_v1.442.sf2",
-    "sf/Piano_Rhodes_73.sf2",
-    "sf/Piano_Yamaha_DX7.sf2",
-    "sf/TimGM6mb.sf2"
+    {
+      file: "sf/AcousticGrandPiano_YDP.sf2",
+      gain: 4
+    },
+    {
+      file: "sf/Arachno.sf2",
+      gain: 4
+    },
+    {
+      file: "sf/FluidR3_GM2-2.sf2",
+      gain: 4
+    },
+    {
+      file: "sf/GeneralUser_GS_MuseScore_v1.442.sf2",
+      gain: 4
+    },
+    {
+      file: "sf/Piano_Rhodes_73.sf2",
+      gain: 10
+    },
+    {
+      file: "sf/Piano_Yamaha_DX7.sf2",
+      gain: 1
+    },
+    {
+      file: "sf/TimGM6mb.sf2",
+      gain: 4
+    }
   ]
 
   sound_fonts.each do |soundfont|
-    sfname = File.basename(soundfont, ".*")
+    sfname = File.basename(soundfont[:file], ".*")
     sfdir = dir + File::SEPARATOR + sfname
     wave = sfdir + File::SEPARATOR + name + ".wav"
     FileUtils::mkdir_p(sfdir)
 
-    `fluidsynth "#{soundfont}" "#{input}" -F "#{wave}" -O float -g 4`
-    #{}`timidity "#{input}" --config-string="soundfont #{soundfont}" -Ow -o "#{wave}"`
+    `fluidsynth "#{soundfont[:file]}" "#{input}" -F "#{wave}" -O float -g #{soundfont[:gain]}`
+    #{}`timidity "#{input}" --config-string="soundfont #{soundfont[:file]}" -Ow -o "#{wave}"`
     wav_to_aac(wave)
     File.delete(wave)
   end
