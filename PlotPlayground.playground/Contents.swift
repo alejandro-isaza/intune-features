06b024535b4e5e41f7aa18655e50f692c1ebbe51
Alejandro Isaza <aisaza@venturemedia.com>
2015-09-28 16:52:08 -0700

    Plot playground improvements

--- PlotPlayground.playground/Contents.swift
+++ PlotPlayground.playground/Contents.swift
@@ -4,7 +4,7 @@ import Surge
 import XCPlayground
 
 //: Set parameters
-let count = 1024
+let count = 2048
 let fs = 44100.0
 let fb = fs / Double(count)
 let fft = FFT(inputLength: count)
@@ -19,12 +19,12 @@ func psd(name: String) -> [Point] {
     var data = [Double](count: count, repeatedValue: 0.0)
     audioFile.readFrames(&data, count: count)
 
-    let psd = fft.forwardMags(data)
+    let psd = sqrt(fft.forwardMags(data))
     return (0..<psd.count).map{ Point(x: fb * Double($0), y: psd[$0]) }
 }
 
 //: Create a PlotView and show it
-let plotView = PlotView(frame: NSRect(x: 0, y: 0, width: 1024, height: 640))
+let plotView = PlotView(frame: NSRect(x: 0, y: 0, width: 1024, height: 400))
 XCPShowView("PSD", view: plotView)
 
 //: Generate a PointSet with the x and y values of the data
@@ -33,7 +33,6 @@ plotView.addPointSet(pointSetC)
 
 //: You can customize the plot intervals
 plotView.fixedXInterval = Interval(min: 0.0, max: 10000)
-plotView.fixedYInterval = Interval(min: 0.0, max: 0.004)
 
 //: You can also overlay mutiple data sets
 let pointSetD = PointSet(points: psd("74"))
