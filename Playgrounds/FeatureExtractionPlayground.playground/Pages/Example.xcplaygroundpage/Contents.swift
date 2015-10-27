e47952237cfb73d3125447310e18167ebb255a29
Alejandro Isaza <aisaza@venturemedia.com>
2015-10-27 16:58:55 -0700

    Update feature data files and network

--- Playgrounds/FeatureExtractionPlayground.playground/Pages/Example.xcplaygroundpage/Contents.swift
+++ Playgrounds/FeatureExtractionPlayground.playground/Pages/Example.xcplaygroundpage/Contents.swift
@@ -7,31 +7,37 @@ import XCPlayground
 //: ## Parameters
 //: Start by selecting the feature and example
 let featureName = "bands"
-let exampleIndex = 395
+let exampleIndex = 1501
+let predictedLabel = 13
 
-//: ## Setup code
-//: No need to touch this
-func readData(filePath: String, datasetName: String) -> [Double] {
-    guard let file = File.open(filePath, mode: .ReadOnly) else {
-        fatalError("Failed to open file")
-    }
-
-    guard let dataset = file.openDataset(datasetName) else {
-        fatalError("Failed to open Dataset")
-    }
-
-    let size = Int(dataset.space.size)
-    var data = [Double](count: size, repeatedValue: 0.0)
-    dataset.readDouble(&data)
-
-    return data
+//: ## Read the feature data from the HDF5 file
+let path = testingFeaturesPath()
+guard let file = File.open(path, mode: .ReadOnly) else {
+    fatalError("Failed to open file")
 }
 
-let path = testingFeaturesPath()
-let labels = readData(path, datasetName: "label")
-let featureData = readData(path, datasetName: featureName)
-
+//: Read the label
+let labelDataset = file.openDataset("label")!
+var labels = [Int](count: Int(labelDataset.space.size), repeatedValue: 0)
+labelDataset.readInt(&labels)
 let label = labels[exampleIndex]
+
+//: Read the file offset
+let offsetDataset = file.openDataset("offset")!
+var offsets = [Int](count: Int(offsetDataset.space.size), repeatedValue: 0)
+offsetDataset.readInt(&offsets)
+let offset = offsets[exampleIndex]
+
+//: Read the file name
+let fileNameDataset = file.openDataset("fileName")!
+var fileNames = fileNameDataset.readString()!
+let fileName = fileNames[exampleIndex]
+
+//: Read the feature data
+let featureDataset = file.openDataset(featureName)!
+var featureData = [Double](count: Int(featureDataset.space.size), repeatedValue: 0.0)
+featureDataset.readDouble(&featureData)
+
 
 //: ## Plotting code
 let plot = PlotView(frame: NSRect(origin: CGPointZero, size: plotSize))
@@ -53,7 +59,7 @@ let points = (0..<exampleSize).map { featureIndex -> PlotKit.Point in
 let pointSet = PointSet(points: points)
 plot.addPointSet(pointSet)
 
-if let note = labelToNote(label) {
+if let note = labelToNote(Double(label)) {
     let expectedBand0 = freqToNote(noteToFreq(Double(note)))
     let expectedBand1 = freqToNote(noteToFreq(Double(note)) * 2)
     let expectedBand2 = freqToNote(noteToFreq(Double(note)) * 3)
@@ -62,8 +68,22 @@ if let note = labelToNote(label) {
         Point(x: expectedBand1, y: 0), Point(x: expectedBand1, y: maxY), Point(x: expectedBand1, y: 0),
         Point(x: expectedBand2, y: 0), Point(x: expectedBand2, y: maxY), Point(x: expectedBand2, y: 0)
     ])
-    expectedPointSet.color = NSColor.lightGrayColor()
+    expectedPointSet.color = NSColor.orangeColor()
+    plot.addPointSet(expectedPointSet)
+}
+
+if let note = labelToNote(Double(predictedLabel)) {
+    let expectedBand0 = freqToNote(noteToFreq(Double(note)))
+    let expectedBand1 = freqToNote(noteToFreq(Double(note)) * 2)
+    let expectedBand2 = freqToNote(noteToFreq(Double(note)) * 3)
+    let expectedPointSet = PointSet(points: [
+        Point(x: expectedBand0, y: 0), Point(x: expectedBand0, y: maxY), Point(x: expectedBand0, y: 0),
+        Point(x: expectedBand1, y: 0), Point(x: expectedBand1, y: maxY), Point(x: expectedBand1, y: 0),
+        Point(x: expectedBand2, y: 0), Point(x: expectedBand2, y: maxY), Point(x: expectedBand2, y: 0)
+        ])
+    expectedPointSet.color = NSColor.purpleColor()
     plot.addPointSet(expectedPointSet)
 }
 
 plot
+XCPlaygroundPage.currentPage.finishExecution()
