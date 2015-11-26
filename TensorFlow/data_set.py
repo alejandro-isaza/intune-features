92673ff3297218e877281ee93f110ca2e0c49691
Aidan Gomez <agomez@venturemedia.com>
2015-11-25 18:21:42 -0800

    Add zero label filtering

--- TensorFlow/data_set.py
+++ TensorFlow/data_set.py
@@ -8,6 +8,29 @@ from __future__ import print_function
 import h5py
 import numpy
 
+class Batch:
+    def __init__(self, batch_size, notes_count):
+        self.count = 0
+        self.zero_count = 0
+        self.non_zero_count = 0
+
+        self.batch_size = batch_size
+        self.notes_count = notes_count
+        self.labels = numpy.ndarray([batch_size, notes_count])
+        self.datasets = numpy.ndarray([batch_size, 4, notes_count])
+
+    def append(self, label, data):
+        self.labels[self.count] = label
+        self.datasets[self.count] = data
+        self.count += 1
+        if label.any():
+            self.non_zero_count += 1
+        else:
+            self.zero_count += 1
+
+    def data(self):
+        return self.datasets.reshape(self.batch_size, 4 * self.notes_count)
+
 
 class DataSet(object):
     def __init__(self, filename):
@@ -33,11 +56,11 @@ class DataSet(object):
     @property
     def example_count(self):
         return self._example_count
-  
+
     @property
     def example_size(self):
         return self._example_size
-  
+
     @property
     def label_size(self):
         return self._label_size
@@ -46,9 +69,32 @@ class DataSet(object):
     def epochs_completed(self):
         return self._epochs_completed
 
+    # def next_batch(self, batch_size):
+    #     """Return the next `batch_size` examples from this data set."""
+    #     start = self._index_in_epoch
+    #     self._index_in_epoch += batch_size
+    #     if self._index_in_epoch > self._example_count:
+    #         # Finished epoch
+    #         self._epochs_completed += 1
+    #         # Start next epoch
+    #         start = 0
+    #         self._index_in_epoch = batch_size
+    #         assert batch_size <= self._example_count
+    #
+    #     end = self._index_in_epoch
+    #     batch = numpy.concatenate((
+    #         self._spectrum[start:end, :],
+    #         self._heights[start:end, :],
+    #         self._locations[start:end, :],
+    #         self._flux[start:end, :]),
+    #         axis=1)
+    #     return batch, self._labels[start:end].astype(float)
+
     def next_batch(self, batch_size):
         """Return the next `batch_size` examples from this data set."""
         start = self._index_in_epoch
+        batch = Batch(batch_size, self._label_size)
+
         self._index_in_epoch += batch_size
         if self._index_in_epoch > self._example_count:
             # Finished epoch
@@ -58,11 +104,20 @@ class DataSet(object):
             self._index_in_epoch = batch_size
             assert batch_size <= self._example_count
 
-        end = self._index_in_epoch
-        batch = numpy.concatenate((
-            self._spectrum[start:end, :],
-            self._heights[start:end, :],
-            self._locations[start:end, :],
-            self._flux[start:end, :]),
-            axis=1)
-        return batch, self._labels[start:end].astype(float)
+        index = 0
+        while batch.count < batch_size:
+            data = numpy.concatenate((
+                self._spectrum[start + index:start + index + 1, :],
+                self._heights[start + index:start + index + 1, :],
+                self._locations[start + index:start + index + 1, :],
+                self._flux[start + index:start + index + 1, :]),
+                axis=0)
+            if (not self._labels[index].any()) & batch.zero_count < batch_size // 2:
+                batch.append(self._labels[index], data)
+            elif self._labels[index].any():
+                batch.append(self._labels[index], data)
+            else:
+                continue
+            index += 1
+
+        return batch.data(), batch.labels
