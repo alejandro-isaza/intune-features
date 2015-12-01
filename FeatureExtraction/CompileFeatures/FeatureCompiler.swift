0ca823c80ec33847fdd43b542147a63b87676497
Aidan Gomez <agomez@venturemedia.com>
2015-12-01 10:58:39 -0800

    Add automatic audio file type deduction
    between mono, poly and noise.

--- FeatureExtraction/CompileFeatures/FeatureCompiler.swift
+++ FeatureExtraction/CompileFeatures/FeatureCompiler.swift
@@ -12,101 +12,179 @@ func midiNoteLabel(notes: Range<Int>, note: Int) -> Int {
 }
 
 class FeatureCompiler {
-    let rootNoisePath = "../AudioData/Noise/"
-    let rootMonoPath = "../AudioData/Monophonic/"
-    let rootPolyPath = "../AudioData/Polyphonic/"
-    let testingFolders = [
-        "Arachno",
-        "VenturePianoQuiet1",
-        "VentureFast1",
-        "mozart",
-        "muss",
-        "alfred40829",
-        "godow",
-        "alfred42458"
+    struct MonophonicFile {
+        let path: String
+        let noteNumber: Int
+    }
+    
+    struct PolyphonicFile {
+        let audioPath: String
+        let midiPath: String
+    }
+    
+    let audioExtensions = [
+        "m4a",
+        "aiff",
+        "mp3",
+        "wav",
+        "caf"
     ]
-
-    var trainingFileName = "training.h5"
-    var testingFileName = "testing.h5"
-    let trainingDatabase: FeatureDatabase
-    let testingDatabase: FeatureDatabase
-
+    
+    let monophonicFileExpression = try! NSRegularExpression(pattern: "/(\\d+)\\.\\w+", options: NSRegularExpressionOptions.CaseInsensitive)
+    
+    let database: FeatureDatabase
+    
     var existingFiles: Set<String>
     var featureBuilder = FeatureBuilder()
+    
+    var polyphonicFiles = [PolyphonicFile]()
+    var monophonicFiles = [MonophonicFile]()
+    var noiseFiles = [String]()
 
-    init(overwrite: Bool) {
-        trainingDatabase = FeatureDatabase(filePath: trainingFileName, overwrite: overwrite)
-        testingDatabase = FeatureDatabase(filePath: testingFileName, overwrite: overwrite)
-        existingFiles = trainingDatabase.fileList.union(testingDatabase.fileList)
+    init(root: String, output: String, overwrite: Bool) {
+        database = FeatureDatabase(filePath: output, overwrite: overwrite)
+        existingFiles = database.fileList
+        let urls = loadFiles(root)
+        (polyphonicFiles, monophonicFiles, noiseFiles) = categorizeURLs(urls)
 
-        print("\nWorking Directory: \(NSFileManager.defaultManager().currentDirectoryPath)\n")
+        print("\nWorking Directory: \(NSFileManager.defaultManager().currentDirectoryPath)")
+        print("Processing \(polyphonicFiles.count) polyphonic + \(monophonicFiles.count) monophonic + \(noiseFiles.count) noise files\n")
     }
 
     func compileNoiseFeatures() {
         let exampleBuilder = MonoExampleBuilder()
+        for (i, file) in noiseFiles.enumerate() {
+            print("Noise: \(i) of \(noiseFiles.count)")
+            guard !existingFiles.contains(file) else {
+                continue
+            }
 
-        var features = [FeatureData]()
-        exampleBuilder.forEachNoiseInFolder(rootNoisePath, action: { example in
-            let featureData = FeatureData(filePath: example.filePath, fileOffset: example.frameOffset, label: example.label)
-            featureData.features = self.featureBuilder.generateFeatures(example)
-            features.append(featureData)
-            self.existingFiles.unionInPlace([example.filePath])
-        })
-
-        trainingDatabase.appendFeatures(features)
-    }
-
-    func compileMonoFeatures() {
-        let folders = loadFolders(rootMonoPath)
-
-        let exampleBuilder = MonoExampleBuilder()
-        for folder in folders {
             var features = [FeatureData]()
-            exampleBuilder.forEachNoteInFolder(folder, action: { example in
+            let label = [Int](count: FeatureBuilder.notes.count, repeatedValue: 0)
+            exampleBuilder.forEachExampleInFile(file, label: label, numExamples: MonoExampleBuilder.numNoiseExamples, action: { example in
                 let featureData = FeatureData(filePath: example.filePath, fileOffset: example.frameOffset, label: example.label)
                 featureData.features = self.featureBuilder.generateFeatures(example)
                 features.append(featureData)
                 self.existingFiles.unionInPlace([example.filePath])
             })
 
-            if testingFolders.contains((folder as NSString).lastPathComponent) {
-                testingDatabase.appendFeatures(features)
-            } else {
-                trainingDatabase.appendFeatures(features)
+            database.appendFeatures(features)
+        }
+    }
+
+    func compileMonoFeatures() {
+        let exampleBuilder = MonoExampleBuilder()
+        for (i, file) in monophonicFiles.enumerate() {
+            print("Mono: \(i) of \(monophonicFiles.count)")
+            guard !existingFiles.contains(file.path) else {
+                continue
             }
+            
+            var features = [FeatureData]()
+            let label = FeatureBuilder.labelForNote(file.noteNumber)
+            exampleBuilder.forEachExampleInFile(file.path, label: label, numExamples: MonoExampleBuilder.numNoteExamples, action: { example in
+                let featureData = FeatureData(filePath: example.filePath, fileOffset: example.frameOffset, label: example.label)
+                featureData.features = self.featureBuilder.generateFeatures(example)
+                features.append(featureData)
+                self.existingFiles.unionInPlace([example.filePath])
+            })
+
+            database.appendFeatures(features)
         }
     }
 
     func compilePolyFeatures() {
-        let folders = loadFolders(rootPolyPath)
-
         let exampleBuilder = PolyExampleBuilder()
-        for folder in folders {
+        for (i, file) in polyphonicFiles.enumerate() {
+            print("Poly: \(i) of \(polyphonicFiles.count)")
+            guard !existingFiles.contains(file.audioPath) else {
+                continue
+            }
+            
             var features = [FeatureData]()
-            exampleBuilder.forEachExampleInFolder(folder, action: { example in
+            exampleBuilder.forEachExampleInAudioFile(file.audioPath, midiFilePath: file.midiPath, action: { example in
                 let featureData = FeatureData(filePath: example.filePath, fileOffset: example.frameOffset, label: example.label)
                 featureData.features = self.featureBuilder.generateFeatures(example)
                 features.append(featureData)
                 self.existingFiles.unionInPlace([example.filePath])
             })
-
-            if testingFolders.contains((folder as NSString).lastPathComponent) {
-                testingDatabase.appendFeatures(features)
-            } else {
-                trainingDatabase.appendFeatures(features)
-            }
+            
+            database.appendFeatures(features)
         }
     }
 
-    func loadFolders(root: String) -> [String] {
-
+    func loadFiles(root: String) -> [NSURL] {
         let fileManager = NSFileManager.defaultManager()
-        guard let folders = try? fileManager.contentsOfDirectoryAtURL(NSURL.fileURLWithPath(root), includingPropertiesForKeys: [NSURLNameKey], options: NSDirectoryEnumerationOptions.SkipsHiddenFiles) else {
+        guard let rootURLs = try? fileManager.contentsOfDirectoryAtURL(NSURL.fileURLWithPath(root), includingPropertiesForKeys: [NSURLNameKey], options: NSDirectoryEnumerationOptions.SkipsHiddenFiles) else {
             fatalError()
         }
-        return folders.map{ $0.path! }
+        
+        var isDirectory: ObjCBool = false
+        var urls = [NSURL]()
+        
+        for url in rootURLs {
+            if fileManager.fileExistsAtPath(url.path!, isDirectory: &isDirectory) && isDirectory {
+                urls.appendContentsOf(loadFiles(url.path!))
+            } else {
+                if audioExtensions.contains(url.pathExtension!) {
+                    urls.append(url)
+                }
+            }
+        }
+        
+        return urls
     }
-
+    
+    func categorizeURLs(urls: [NSURL]) -> (polyphonic: [PolyphonicFile], monophonic: [MonophonicFile], noise: [String]) {
+        var polyphonic = [PolyphonicFile]()
+        var monophonic = [MonophonicFile]()
+        var noise = [String]()
+        
+        for url in urls {
+            if let polyPath = polyPath(url) {
+                polyphonic.append(polyPath)
+            } else if let monoPath = monoPath(url) {
+                monophonic.append(monoPath)
+            } else {
+                noise.append(url.path!)
+            }
+        }
+        
+        return (polyphonic: polyphonic, monophonic: monophonic, noise: noise)
+    }
+    
+    func monoPath(url: NSURL) -> MonophonicFile? {
+        let path = url.path!
+        
+        guard let results = monophonicFileExpression.firstMatchInString(path, options: NSMatchingOptions.ReportCompletion, range: NSMakeRange(0, path.characters.count)) else {
+            return nil
+        }
+        if results.numberOfRanges < 1 {
+            return nil
+        }
+        let range = results.rangeAtIndex(1)
+        
+        let fileName = (path as NSString).substringWithRange(range)
+        guard let noteNumber = Int(fileName) else {
+            return nil
+        }
+        
+        return MonophonicFile(path: path, noteNumber: noteNumber)
+    }
+    
+    func polyPath(url: NSURL) -> PolyphonicFile? {
+        let manager = NSFileManager.defaultManager()
+        guard let midFile = url.URLByDeletingPathExtension?.URLByAppendingPathExtension("mid") else {
+            fatalError("Failed to build path")
+        }
+        
+        if manager.fileExistsAtPath(midFile.path!) {
+            return PolyphonicFile(audioPath: url.path!, midiPath: midFile.path!)
+        }
+        
+        return nil
+    }
+    
     func shuffle(chunkSize chunkSize: Int, passes: Int) {
         let isTTY = isatty(fileno(stdin)) != 0
         let eraseLastLineCommand = "\u{1B}[A\u{1B}[2K"
@@ -115,15 +193,9 @@ class FeatureCompiler {
             print("Shuffling...")
         }
 
-        trainingDatabase.shuffle(chunkSize: chunkSize, passes: passes) { progress in
+        database.shuffle(chunkSize: chunkSize, passes: passes) { progress in
             if isTTY {
-                print("\(eraseLastLineCommand)Shuffling training data...\(round(progress * 10000) / 100)%")
-            }
-        }
-
-        testingDatabase.shuffle(chunkSize: chunkSize, passes: passes) { progress in
-            if isTTY {
-                print("\(eraseLastLineCommand)Shuffling testing data...\(round(progress * 10000) / 100)%")
+                print("\(eraseLastLineCommand)Shuffling database data...\(round(progress * 10000) / 100)%")
             }
         }
     }
--- FeatureExtraction/CompileFeatures/MonoExampleBuilder.swift
+++ FeatureExtraction/CompileFeatures/MonoExampleBuilder.swift
@@ -13,8 +13,8 @@ class MonoExampleBuilder {
         "aiff"
     ]
     
-    let numNoteExamples = 15
-    let numNoiseExamples = 1000
+    static let numNoteExamples = 15
+    static let numNoiseExamples = 1000
 
     private var data: (RealArray, RealArray)
     
@@ -26,7 +26,7 @@ class MonoExampleBuilder {
     func forEachNoteInFolder(folder: String, action: Example -> ()) {
         for note in FeatureBuilder.notes {
             let label = FeatureBuilder.labelForNote(note)
-            forEachExampleInFile(String(note), path: folder, label: label, numExamples: numNoteExamples, action: action)
+            forEachExampleInFile(String(note), path: folder, label: label, numExamples: MonoExampleBuilder.numNoteExamples, action: action)
         }
         print("")
     }
@@ -42,7 +42,7 @@ class MonoExampleBuilder {
         for file in files {
             let filePath = file.path!
             print("Processing \(filePath)")
-            forEachExampleInFile(filePath, label: label, numExamples: numNoiseExamples, action: action)
+            forEachExampleInFile(filePath, label: label, numExamples: MonoExampleBuilder.numNoiseExamples, action: action)
         }
         print("")
     }
@@ -71,7 +71,9 @@ class MonoExampleBuilder {
         }
 
         let audioFile = AudioFile.open(filePath)!
-        assert(audioFile.sampleRate == FeatureBuilder.samplingFrequency)
+        guard audioFile.sampleRate == FeatureBuilder.samplingFrequency else {
+            fatalError("Sample rate mismatch: \(filePath) => \(audioFile.sampleRate) != \(FeatureBuilder.samplingFrequency)")
+        }
         guard audioFile.readFrames(data.1.mutablePointer + overlap, count: step) == step else {
             return
         }
