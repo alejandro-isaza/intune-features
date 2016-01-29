97f1be83386afd6fea8ad13817ef13158c7815bf
Aidan Gomez <agomez@venturemedia.com>
2016-01-28 21:31:58 -0800

    Update rnn structure

--- TensorFlow/rnn/onset_net/data_set.py
+++ TensorFlow/rnn/onset_net/data_set.py
@@ -7,8 +7,13 @@ class DataSet:
         self.file_name = file_name
         self.h5_file = h5.File(file_name, "r")
         self.present_index = 0
+        self.feature_size = 400
+        self.sample_count = self.h5_file["features_length"].shape[0]
 
-    def next_batch(batch_size):
+    def next_batch(self, batch_size):
+        if (self.present_index + batch_size > self.sample_count):
+            self.present_index = 0
+
         features_length_d = self.h5_file["features_length"]
         features_onset_values_d = self.h5_file["features_onset_values"]
 
@@ -17,20 +22,13 @@ class DataSet:
         spectrum_d = self.h5_file["spectrum"]
         spectrum_flux_d = self.h5_file["spectrum_flux"]
 
-        sequence_sizes = features_length_d[self.present_index:self.present_index+batch_size]
-        sequences = []
-        labels = []
-        for i in xrange(batch_size):
-            size = sequence_sizes[i]
-            sequence = np.concatenate((spectrum_d[self.present_index, 0:size, :],
-                                       spectrum_flux_d[self.present_index, 0:size, :],
-                                       peak_heights_d[self.present_index, 0:size, :],
-                                       peak_locations_d[self.present_index, 0:size, :]), axis=2)
-            label = features_onset_values_d[self.present_index, 0:size, :]
+        sequences = np.concatenate((spectrum_d[self.present_index:self.present_index+batch_size, :, :],
+                                   spectrum_flux_d[self.present_index:self.present_index+batch_size, :, :],
+                                   peak_heights_d[self.present_index:self.present_index+batch_size, :, :],
+                                   peak_locations_d[self.present_index:self.present_index+batch_size, :, :]), axis=2)
+        labels = features_onset_values_d[self.present_index:self.present_index+batch_size, :]
+        sequence_lengths = features_length_d[self.present_index:self.present_index+batch_size]
 
-            sequences.append(sequence)
-            labels.append(label)
+        self.present_index += batch_size
 
-            self.present_index += 1
-
-        return sequences, labels
+        return sequences, labels, sequence_lengths
--- TensorFlow/rnn/onset_net/runner.py
+++ TensorFlow/rnn/onset_net/runner.py
@@ -12,30 +12,40 @@ num_steps = 40
 lstm_units = 20
 layer_count = 3
 
+max_sequence_length = 43
+
 train_data = DataSet("training.h5")
 test_data = DataSet("testing.h5")
 
-def fill_batch_vars(dataset, feature_var, label_var):
-    data, label = dataset.next_batch(batch_size)
+def fill_batch_vars(dataset, feature_var, label_var, length_var):
+    data, label, length = dataset.next_batch(batch_size)
     feed_dict = {
         feature_var: data,
-        label_var: label
+        label_var: label,
+        length_var: length
     }
     return feed_dict
 
 if __name__ == '__main__':
     with tf.Graph().as_default(), tf.Session() as sess:
-        features_placeholder = tf.placeholder(tf.float32)
+        features_placeholder = tf.placeholder(tf.float32, shape=(batch_size, max_sequence_length, train_data.feature_size))
+        sequence_lengths = tf.placeholder(tf.int64, shape=(batch_size))
         labels_placeholder = tf.placeholder(tf.float32)
 
+        W = tf.Variable(tf.zeros([2 * lstm_units, 1]))
+        b = tf.Variable(tf.zeros([1]))
+
         f_lstm = rnn_cell.BasicLSTMCell(lstm_units)
-        f_stacked_lstm = rnn_cell.MultiRNNCell([lstm] * layer_count)
+        f_stacked_lstm = rnn_cell.MultiRNNCell([f_lstm] * layer_count)
 
         r_lstm = rnn_cell.BasicLSTMCell(lstm_units)
-        r_stacked_lstm = rnn_cell.MultiRNNCell([lstm] * layer_count)
+        r_stacked_lstm = rnn_cell.MultiRNNCell([r_lstm] * layer_count)
 
-        logits = rnn.bidirectional_rnn(f_stacked_lstm, r_stacked_lstm, features_placeholder)
-        loss = tf.nn.softmax_cross_entropy_with_logits(logits, labels_placeholder)
+        features = [tf.squeeze(t) for t in tf.split(1, max_sequence_length, features_placeholder)]
+
+        rnn_out = rnn.bidirectional_rnn(f_stacked_lstm, r_stacked_lstm, features, dtype=tf.float32, sequence_length=sequence_lengths)
+        logits = tf.concat(1, [tf.matmul(t, W) + b for t in rnn_out])[:, :]
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, labels_placeholder))
 
 
         optimizer = tf.train.GradientDescentOptimizer(learning_rate)
@@ -47,11 +57,11 @@ if __name__ == '__main__':
 
         # REMINDER: labels needs to be shaped like [time][batch_size][fw_output + bw_output]
         for i in xrange(max_epoch):
-            feed_dict = fill_batch_vars(train_data, features_placeholder, labels_placeholder)
+            feed_dict = fill_batch_vars(train_data, features_placeholder, labels_placeholder, sequence_lengths)
             _, batch_loss = sess.run([train_op, loss], feed_dict=feed_dict)
             print("Batch loss: %d" % (batch_loss))
 
             if i % 100 == 0:
-                feed_dict = fill_batch_vars(test_data, features_placeholder, labels_placeholder)
+                feed_dict = fill_batch_vars(test_data, features_placeholder, labels_placeholder, sequence_lengths)
                 test_loss = sess.run([loss], feed_dict=feed_dict)
-                print("    Testing loss: %d" % (test_loss))
+                print("    Testing loss: %d" % (test_loss[0]))
