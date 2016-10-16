e606d170e784f4f43e587784da494c4b6744aff9
Alejandro Isaza <al@isaza.ca>
2016-10-16 16:02:11 -0700

    Switch to using submodules

--- Tests/NoteTests.swift
+++ Tests/NoteTests.swift
@@ -14,22 +14,22 @@ class NoteTests: XCTestCase {
         let C1 = Note(midiNoteNumber: 24)
         XCTAssertFalse(C1.isSharp)
         XCTAssertEqual(C1.octave, 1)
-        XCTAssertEqual(C1.note, Note.NoteType.C)
+        XCTAssertEqual(C1.note, Note.NoteType.c)
 
         let Cs1 = Note(midiNoteNumber: 25)
         XCTAssertTrue(Cs1.isSharp)
         XCTAssertEqual(Cs1.octave, 1)
-        XCTAssertEqual(Cs1.note, Note.NoteType.CSharp)
+        XCTAssertEqual(Cs1.note, Note.NoteType.cSharp)
 
         let E4 = Note(midiNoteNumber: 64)
         XCTAssertFalse(E4.isSharp)
         XCTAssertEqual(E4.octave, 4)
-        XCTAssertEqual(E4.note, Note.NoteType.E)
+        XCTAssertEqual(E4.note, Note.NoteType.e)
 
         let C7 = Note(midiNoteNumber: 96)
         XCTAssertFalse(C7.isSharp)
         XCTAssertEqual(C7.octave, 7)
-        XCTAssertEqual(C7.note, Note.NoteType.C)
+        XCTAssertEqual(C7.note, Note.NoteType.c)
     }
 
 }
